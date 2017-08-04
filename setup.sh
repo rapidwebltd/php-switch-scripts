7ff8bd8b04acdc4d33be2993e38ca6cf90309d63
Jordan Hall <jordan.hall@rapidweb.biz>
2017-08-04 13:11:47 +0100

    Suppress addional PHP extension output

--- setup.sh
+++ setup.sh
@@ -19,6 +19,6 @@ echo "* Installing PHP 7.0 extensions..."
 sudo apt-get install -y php7.0-gd php7.0-mysql php7.0-sqlite php7.0-soap php7.0-xsl php7.0-json php7.0-opcache php7.0-mbstring php7.0-readline php7.0-curl php7.0-mcrypt php7.0-xml php7.0-zip > /dev/null
 
 echo "* Installing additional PHP extensions..."
-sudo apt-get install -y php-memcache php-memcached 
+sudo apt-get install -y php-memcache php-memcached  > /dev/null
 
 echo "* Setup complete. You may now use the 'switch-to-php-*.*.sh' scripts."
