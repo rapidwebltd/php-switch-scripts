57fb89551f3b990f39e99e1bd0134fc2cf1e162a
Jordan Hall <jordan.hall@rapidweb.biz>
2017-08-04 13:23:47 +0100

    Typo

--- switch-to-php-5.6.sh
+++ switch-to-php-5.6.sh
@@ -9,7 +9,7 @@ sudo a2enmod php5.6 > /dev/null
 echo "* Restarting Apache..."
 sudo service apache2 restart > /dev/null
 
-echo "* Switching CLI PHP to 7.0..."
+echo "* Switching CLI PHP to 5.6..."
 sudo update-alternatives --set php /usr/bin/php5.6 > /dev/null
 
 echo "* Switch to PHP 5.6 complete."
