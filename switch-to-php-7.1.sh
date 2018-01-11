272f0b935dc9efd6e0fbe94363b8280553e13d6e
Bohdan Tsekhanskyi <kirweb@gmail.com>
2018-01-11 17:02:35 +0200

    Added script for switch php version to the 7.1

---
+++ switch-to-php-7.1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "* Disabling Apache PHP 5.6 module..."
+sudo a2dismod php5.6 > /dev/null
+
+echo "* Enabling Apache PHP 7.0 module..."
+sudo a2enmod php7.1 > /dev/null
+
+echo "* Restarting Apache..."
+sudo service apache2 restart > /dev/null
+
+echo "* Switching CLI PHP to 7.1..."
+sudo update-alternatives --set php /usr/bin/php7.1 > /dev/null
+
+echo "* Switch to PHP 7.0 complete."
