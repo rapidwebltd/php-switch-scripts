14a5520bdd834748ee8c37999551e16df8d9b438
Nafies Luthfi <nafiesl@gmail.com>
2019-03-23 09:16:48 +0800

    Add php 7.3 mod disables

--- switch-to-php-7.2.sh
+++ switch-to-php-7.2.sh
@@ -9,6 +9,9 @@ sudo a2dismod php7.0 > /dev/null
 echo "* Disabling Apache PHP 7.1 module..."
 sudo a2dismod php7.1 > /dev/null
 
+echo "* Disabling Apache PHP 7.3 module..."
+sudo a2dismod php7.3 > /dev/null
+
 echo "* Enabling Apache PHP 7.2 module..."
 sudo a2enmod php7.2 > /dev/null
 
