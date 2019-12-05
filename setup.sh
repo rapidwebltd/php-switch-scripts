a477928c0fb37a0be7d12892a13aa708c55b0342
Bohdan Tsekhanskyi <kirweb@gmail.com>
2019-12-05 21:56:55 +0200

    Added the switch to PHP7.3 version.

--- setup.sh
+++ setup.sh
@@ -42,6 +42,12 @@ sudo apt-get install -y php7.3 php7.3-common php7.3-cli > /dev/null
 echo "* Installing PHP 7.3 extensions..."
 sudo apt-get install -y php7.3-bz2 php7.3-curl php7.3-gd php7.3-json php7.3-mbstring php7.3-mysql php7.3-opcache php7.3-readline php7.3-soap php7.3-sqlite3 php7.3-tidy php7.3-xml php7.3-xsl php7.3-zip > /dev/null
 
+echo "* Installing PHP 7.4..."
+sudo apt-get install -y php7.4 php7.4-common php7.4-cli > /dev/null
+
+echo "* Installing PHP 7.4 extensions..."
+sudo apt-get install -y php7.4-bz2 php7.4-curl php7.4-gd php7.4-json php7.4-mbstring php7.4-mysql php7.4-opcache php7.4-readline php7.4-soap php7.4-sqlite3 php7.4-tidy php7.4-xml php7.4-xsl php7.4-zip > /dev/null
+
 echo "* Installing additional PHP extensions..."
 sudo apt-get install -y php-memcache php-memcached php-redis  > /dev/null
 
